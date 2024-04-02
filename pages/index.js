648abb17dd36731b7be9cb5c59af6b0629acfcfa
Anderson Santos <42190719+andersonh-santos@users.noreply.github.com>
2024-04-02 20:56:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Quando você olha muito tempo para um abismo, o abismo olha para você</h1>
+function Home() {
+  return (
+    <h1>
+      Quando você olha muito tempo para um abismo, o abismo olha para você
+    </h1>
+  );
 }
 
 export default Home;
